84f14a1a4f9b1c120670a1c91a24b397b9545e5e
Fred Sauer <fredsa@google.com>
2016-07-20 16:24:56 -0700

    delete only empty lists

--- app/src/main/java/sauer/lists/ListOfListsActivity.java
+++ app/src/main/java/sauer/lists/ListOfListsActivity.java
@@ -1,15 +1,19 @@
 package sauer.lists;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 
-public class ListOfListsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+import java.util.Random;
+
+public class ListOfListsActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 
     ListView listView;
 
@@ -19,12 +23,21 @@ public class ListOfListsActivity extends AppCompatActivity implements AdapterVie
 
         setContentView(R.layout.activity_list_of_lists);
 
-        DatabaseReference lists = Store.lists();
+        final DatabaseReference lists = Store.lists();
 
         listView = (ListView) findViewById(R.id.list_view);
         ListOfListsAdapter adapter = new ListOfListsAdapter(getApplicationContext(), R.layout.named_list, lists);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
+        listView.setOnItemLongClickListener(this);
+
+        FloatingActionButton addButton = (FloatingActionButton) findViewById(R.id.add_button);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                lists.child("" + Math.round(1000 * Math.random())).setValue("");
+            }
+        });
     }
 
     @Override
@@ -33,5 +46,14 @@ public class ListOfListsActivity extends AppCompatActivity implements AdapterVie
         Intent intent = new Intent(this, ListOfItemsActivity.class);
         intent.putExtra("key", namedList.databaseReference.getKey());
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long l) {
+        NamedList namedList = (NamedList) listView.getItemAtPosition(position);
+        if (namedList.getCount() == 0) {
+            namedList.databaseReference.removeValue();
+        }
+        return true;
     }
 }
