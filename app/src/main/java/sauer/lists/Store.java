74612e9fb72c84eec8131806bccffc856605aefe
Fred Sauer <fredsa@google.com>
2016-08-19 13:07:54 -0700

    work offline and log connected/disconnected status

--- app/src/main/java/sauer/lists/Store.java
+++ app/src/main/java/sauer/lists/Store.java
@@ -1,9 +1,18 @@
 package sauer.lists;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Store {
+
+    private static final String TAG = Store.class.getName() + " *******";
+
+    private static final String INFO_CONNECTED = ".info/connected";
 
     static FirebaseDatabase database;
     static DatabaseReference lists;
@@ -12,8 +21,21 @@ public class Store {
     static void init() {
         if (database == null) {
             database = FirebaseDatabase.getInstance();
+            database.setPersistenceEnabled(true);
             lists = database.getReference("lists");
             acls = database.getReference("acls");
+            database.getReference(INFO_CONNECTED).addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot snapshot) {
+                    boolean connected = snapshot.getValue(Boolean.class);
+                    Log.d(TAG, INFO_CONNECTED + "--> " + (connected ? "CONNECTED" : "DISCONNECTED"));
+                }
+
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    Log.d(TAG, INFO_CONNECTED + "--> onCancelled()");
+                }
+            });
         }
     }
 
