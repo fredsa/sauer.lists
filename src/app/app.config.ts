8195fa0b1e2a9f7885af070f6209b58e3b884985
Fred Sauer <fredsa@google.com>
2024-06-20 16:01:18 -0700

    ng add @angular/fire
    Authentication, Firestore, Cloud Functions (callable)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,27 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+  provideRouter(routes),
+  provideAnimationsAsync(),
+  provideFirebaseApp(() => initializeApp({
+    "projectId": "sauer-lists",
+    "appId": "1:460983314604:web:808b1007204e4c2ea4d709",
+    "databaseURL": "https://sauer-lists.firebaseio.com",
+    "storageBucket": "sauer-lists.appspot.com",
+    "locationId": "us-central",
+    "apiKey": "AIzaSyDsokPvdQq8v1w6qIaRITDPg82QIfgNIiE",
+    "authDomain": "sauer-lists.firebaseapp.com",
+    "messagingSenderId": "460983314604",
+  })),
+  provideAuth(() => getAuth()),
+  provideFirestore(() => getFirestore()),
+  provideFunctions(() => getFunctions()),
+  ]
 };
